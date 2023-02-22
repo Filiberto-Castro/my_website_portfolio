74126e40f3d1c5213e43173463bb47ca1bb18420
Filiberto_Castro <fcastrob@miumg.edu.gt>
2023-02-21 18:24:30 -0600

    Trabajando el header

---
+++ my_portfolio-website/src/components/header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.jpg'
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>Filiberto Castro</h1>
+        <h5 className='text-light'>Android Developer</h5>
+        <CTA />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className='scroll_down'>Scroll Down</a>
+
+
+      </div>
+    </header>
+  )
+}
+
+export default Header
